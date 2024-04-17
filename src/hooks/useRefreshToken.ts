9839967a8f358ad7538f7e6ac9fcae98e41b2259
Yeldar <57894795+issa012@users.noreply.github.com>
2024-04-17 21:18:51 +0500

    fix: fix axios interceptors

--- src/hooks/useRefreshToken.ts
+++ src/hooks/useRefreshToken.ts
@@ -5,15 +5,15 @@ const useRefreshToken = () => {
   const { setToken } = useAuth();
 
   const refresh = async () => {
-    const response = await authApi.get("/refresh", {
+    const response = await authApi.post("api/refresh", {
       withCredentials: true,
     });
     setToken((prev) => {
       console.log(JSON.stringify(prev));
-      console.log(response.data.accessToken);
-      return response.data.accessToken;
+      console.log(response.data.token);
+      return response.data.token;
     });
-    return response.data.accessToken;
+    return response.data.token;
   };
   return refresh;
 };
