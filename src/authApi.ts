03f625ec70f9a9ec63e83f1ace93d54985f0d706
Yeldar <57894795+issa012@users.noreply.github.com>
2024-04-19 20:41:51 +0500

    change url

--- src/authApi.ts
+++ src/authApi.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-// const BASE_URL = "http://130.211.94.133:1337";
-const BASE_URL = "http://localhost:1337";
+const BASE_URL = "http://130.211.94.133:1337";
+// const BASE_URL = "http://localhost:1337";
 
 export const authApi = axios.create({ baseURL: BASE_URL, withCredentials: true });
 authApi.defaults.headers.common["Content-Type"] = "application/json";
