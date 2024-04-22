9e4e4b71507ed280334d62170fe1d05f27196d70
Yeldar <57894795+issa012@users.noreply.github.com>
2024-04-22 21:12:35 +0500

    feat: pagination and new design for job page

--- src/pages/JobBoard/index.tsx
+++ src/pages/JobBoard/index.tsx
@@ -3,16 +3,32 @@ import useAxiosPrivate from "@/hooks/useAxiosPrivate";
 import { useQuery } from "@tanstack/react-query";
 import JobCard from "./job-card";
 
+import {
+  Pagination,
+  PaginationContent,
+  PaginationEllipsis,
+  PaginationItem,
+  PaginationLink,
+  PaginationNext,
+  PaginationPrevious,
+} from "@/components/ui/pagination";
+import { ScrollRestoration, useSearchParams } from "react-router-dom";
+
 const JobBoard = () => {
+  const [searchParams] = useSearchParams();
+  const currentPage = searchParams.get("page") ?? 1;
   const axiosPrivate = useAxiosPrivate();
 
+  const fetchJobs = async (page: number | string) => {
+    const res = await axiosPrivate.get(`api/vacancy/?page=${page}`);
+    return res.data;
+  };
+
   const { data, isLoading, isFetching } = useQuery({
-    queryKey: ["vacancies"],
-    queryFn: async () => {
-      const res = await axiosPrivate.get("api/vacancy");
-      return res.data;
-    },
+    queryKey: ["vacancies", currentPage],
+    queryFn: () => fetchJobs(currentPage),
   });
+
   if (isLoading || isFetching) return <FullScreenLoading />;
 
   return (
@@ -23,6 +39,29 @@ const JobBoard = () => {
           <JobCard job={job} key={job.id} />
         ))}
       </div>
+      <Pagination>
+        <PaginationContent>
+          <PaginationItem>
+            <PaginationPrevious to={`?page=${+currentPage - 1}`} />
+          </PaginationItem>
+          <PaginationItem>
+            <PaginationLink to="?page=1">1</PaginationLink>
+          </PaginationItem>
+          <PaginationItem>
+            <PaginationLink to="?page=2">2</PaginationLink>
+          </PaginationItem>
+          <PaginationItem>
+            <PaginationLink to="?page=3">3</PaginationLink>
+          </PaginationItem>
+          <PaginationItem>
+            <PaginationEllipsis />
+          </PaginationItem>
+          <PaginationItem>
+            <PaginationNext to={`?page=${+currentPage + 1}`} />
+          </PaginationItem>
+        </PaginationContent>
+      </Pagination>
+      <ScrollRestoration />
     </div>
   );
 };
--- src/pages/JobBoard/job-card.tsx
+++ src/pages/JobBoard/job-card.tsx
@@ -1,9 +1,15 @@
 import { Link } from "react-router-dom";
 
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
-import { Textarea } from "@/components/ui/textarea";
+import { useRef } from "react";
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+import { useTruncatedElement } from "@/hooks/useTruncatedElement";
 
 const JobCard = ({ job }: { job: IJob }) => {
+  const ref = useRef(null);
+  const { isTruncated, isShowingMore, toggleIsShowingMore } = useTruncatedElement({ ref });
+
   return (
     <Link key={job.id} to="#">
       <Card className="hover:bg-muted">
@@ -14,9 +20,22 @@ const JobCard = ({ job }: { job: IJob }) => {
           </CardDescription>
         </CardHeader>
         <CardContent>
-          <Textarea readOnly={true} className="w-full resize-none bg-inherit" rows={8}>
+          <div
+            className={cn(
+              "w-full resize-none bg-inherit whitespace-pre-wrap line-clamp-4",
+              isShowingMore && "line-clamp-none"
+            )}
+            ref={ref}
+          >
             {job.description}
-          </Textarea>
+          </div>
+          {isTruncated && (
+            <div className="text-center">
+              <Button variant="ghost" onClick={toggleIsShowingMore} className="hover:underline">
+                {isShowingMore ? "Show less" : "Show more"}
+              </Button>
+            </div>
+          )}
         </CardContent>
       </Card>
     </Link>
