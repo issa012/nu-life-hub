dc690aef7c2c4289600c607e2cd4cfe669bf0123
Yeldar <57894795+issa012@users.noreply.github.com>
2024-04-22 17:37:42 +0500

    style: new style for marketplace item

--- src/pages/Marketplace/item.tsx
+++ src/pages/Marketplace/item.tsx
@@ -1,60 +1,39 @@
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
-import { Textarea } from "@/components/ui/textarea";
 import { Link } from "react-router-dom";
 
 const Item = ({ item }: { item: IItem }) => {
   return (
-    <div>
-      <div className="grid grid-cols-[210px_1fr] rounded-sm border mt-4 p-4">
-        <div className="flex justify-center">
-          <img className="aspect-auto object-cover max-h-44" src={item.image_url} />
-        </div>
+    <li className="w-full sm:w-1/2 md:w-1/2 lg:w-1/3 xl:w-1/4 p-2 pb-[2px] bg-white">
+      <div className="h-full rounded-lg pb-3">
         <div className="">
-          <div className="flex justify-between mb-4">
-            <span className="text-2xl font-semibold tracking-tight">{item.name}</span>
-            <span className="text-2xl">{item.price} tenge</span>
+          <div className="relative rounded-lg overflow-hidden h-full flex justify-center before:block before:content=[''] before:pt-[100%]">
+            <Link to={`${item.id}`}>
+              <div className="absolute top-0 left-0 right-0 bottom-0 flex justify-center items-center h-full w-full">
+                <img
+                  className="inline-block max-h-full max-w-full flex-shrink-0 h-full w-full object-contain bg-muted"
+                  src={item.image_url}
+                />
+              </div>
+            </Link>
           </div>
-          <div>
-            <Textarea className="bg-inherit resize-none" rows={5}>
-              {item.description}
-            </Textarea>
+          <div className="my-2 mx-1 w-auto">
+            <Link to={`${item.id}`} className="hover:underline">
+              <h3 className="text-sm line-clamp-3">{item.name}</h3>
+            </Link>
+            <div>
+              <div>
+                <span className="font-bold text-base">{item.price} tenge</span>
+              </div>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  );
-  return (
-    <Link key={item.name} to="#">
-      <Card className="hover:bg-muted mt-5 max-w-80">
-        <CardHeader>
-          <CardTitle>
-            <div className="flex justify-between">
-              <span>{item.name}</span>
-              <span className="font-normal">{item.price}</span>
-            </div>
-          </CardTitle>
-          <CardDescription>
-            <span>
-              Posted: {item.created_date} by <span className="hover:text-primary">{item.user}</span>
-            </span>
-            <span></span>
-          </CardDescription>
-        </CardHeader>
-        <CardContent>
-          <div>
-            <div>
-              <img src={item.image_url} />
-            </div>
-            <div className="mt-4">{item.description}</div>
-          </div>
-        </CardContent>
-      </Card>
-    </Link>
+    </li>
   );
 };
 export default Item;
 
 interface IItem {
+  id: string;
   user: string;
   name: string;
   description: string;
