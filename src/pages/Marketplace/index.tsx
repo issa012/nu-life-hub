75cb0e66b0cafd0c6e919eb6ee3dce891d91ea51
Yeldar <57894795+issa012@users.noreply.github.com>
2024-04-22 19:48:11 +0500

    add pagination to marketplace

--- src/pages/Marketplace/index.tsx
+++ src/pages/Marketplace/index.tsx
@@ -1,48 +1,79 @@
-import { useQuery } from "@tanstack/react-query";
-import Item from "./item";
+import { ScrollRestoration } from "react-router-dom";
+import { keepPreviousData, useQuery } from "@tanstack/react-query";
+import { useSearchParams } from "react-router-dom";
+
 import useAxiosPrivate from "@/hooks/useAxiosPrivate";
 import FullScreenLoading from "@/components/fullscreen-loading";
 import { CreateItem } from "./create-item";
+import Item from "./item";
+
+import {
+  Pagination,
+  PaginationContent,
+  PaginationEllipsis,
+  PaginationItem,
+  PaginationLink,
+  PaginationNext,
+  PaginationPrevious,
+} from "@/components/ui/pagination";
 
 const Marketplace = () => {
+  const [searchParams] = useSearchParams();
+  const currentPage = searchParams.get("page") ?? 1;
   const apiClient = useAxiosPrivate();
 
+  const fetchItems = async (page: number | string) => {
+    const response = await apiClient.get(`api/item/?page=${page}`);
+    return response.data;
+  };
+
   const { data, isLoading, isFetching } = useQuery({
-    queryKey: ["marketplace-items"],
-    queryFn: async () => {
-      const response = await apiClient.get("api/item");
-      return response.data;
-    },
+    queryKey: ["marketplace-items", currentPage],
+    queryFn: () => fetchItems(currentPage),
+    placeholderData: keepPreviousData,
   });
 
   if (isLoading || isFetching) return <FullScreenLoading />;
 
+  console.log(data);
+  console.log(currentPage);
   return (
     <div>
       <h1 className="scroll-m-20 pb-2 text-xl font-light tracking-tight">Marketplace</h1>
-      <div className="grid grid-cols-[1fr_auto]">
-        <div className="max-w-screen-md">
+      <div>
+        <CreateItem />
+      </div>
+      <div className="">
+        <ul className="flex flex-row flex-wrap list-none">
           {data.results.map((item) => (
             <Item item={item} key={item.id} />
           ))}
-        </div>
-        <div>
-          <CreateItem />
-        </div>
+        </ul>
       </div>
+      <Pagination>
+        <PaginationContent>
+          <PaginationItem>
+            <PaginationPrevious to={`?page=${+currentPage - 1}`} />
+          </PaginationItem>
+          <PaginationItem>
+            <PaginationLink to="?page=1">1</PaginationLink>
+          </PaginationItem>
+          <PaginationItem>
+            <PaginationLink to="?page=2">2</PaginationLink>
+          </PaginationItem>
+          <PaginationItem>
+            <PaginationLink to="?page=3">3</PaginationLink>
+          </PaginationItem>
+          <PaginationItem>
+            <PaginationEllipsis />
+          </PaginationItem>
+          <PaginationItem>
+            <PaginationNext to={`?page=${+currentPage + 1}`} />
+          </PaginationItem>
+        </PaginationContent>
+      </Pagination>
+      <ScrollRestoration />
     </div>
   );
 };
 export default Marketplace;
-
-const items = [
-  {
-    user: "aaa",
-    name: "Hello Kitty plushie",
-    description: "Hello kitty Plushie soft 2x2m size",
-    price: "1000tg",
-    category: "Items",
-    image_url: "#",
-    created_date: new Date().toDateString(),
-  },
-];
