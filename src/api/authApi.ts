e3700fd1958f2446020e08b12412e1ae639edcd9
Yeldar <57894795+issa012@users.noreply.github.com>
2024-04-26 14:39:42 +0500

    fix: change location

--- src/api/authApi.ts
+++ src/api/authApi.ts
@@ -2,7 +2,6 @@ import axios from "axios";
 import createAuthRefreshInterceptor from "axios-auth-refresh";
 
 const BASE_URL = "http://130.211.94.133:1337";
-// const BASE_URL = "http://localhost:1337";
 
 export const authApi = axios.create({ baseURL: BASE_URL, withCredentials: true });
 authApi.defaults.headers.common["Content-Type"] = "application/json";
