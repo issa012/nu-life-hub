3e05a27a5a2ae5a0885e3ecc2818c5edf2b9db38
Yeldar <57894795+issa012@users.noreply.github.com>
2024-04-26 19:06:01 +0500

    add edit modal for profile page

--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -1,17 +1,37 @@
 import FullScreenLoading from "@/components/fullscreen-loading";
 import useUser from "@/hooks/useUser";
+import { EditProfileDialog } from "./edit";
 
 const UserProfile = () => {
-  const { data, isLoading, isFetching } = useUser();
+  const { data, isLoading } = useUser();
 
-  if (isLoading || isFetching) return <FullScreenLoading />;
+  if (isLoading) return <FullScreenLoading />;
   console.log(data);
   return (
     <div>
-      {/* <h3 className="scroll-m-20 text-2xl font-light tracking-tight">Profile settings</h3> */}
+      <div className="flex gap-10">
+        <h3 className="scroll-m-20 text-2xl font-light tracking-tight">Profile settings</h3>
+        <EditProfileDialog />
+      </div>
       <div>
         <table className="w-fit mt-4">
           <tbody>
+            <tr className="">
+              <td className="px-4 py-2 text-left [&[align=center]]:text-center [&[align=right]]:text-right">
+                First name
+              </td>
+              <td className="px-4 py-2 text-left [&[align=center]]:text-center [&[align=right]]:text-right">
+                {data?.first_name}
+              </td>
+            </tr>
+            <tr className="">
+              <td className="px-4 py-2 text-left [&[align=center]]:text-center [&[align=right]]:text-right">
+                Last name
+              </td>
+              <td className="px-4 py-2 text-left [&[align=center]]:text-center [&[align=right]]:text-right">
+                {data?.last_name}
+              </td>
+            </tr>
             <tr className="">
               <td className="px-4 py-2 text-left [&[align=center]]:text-center [&[align=right]]:text-right">
                 Username
@@ -26,6 +46,14 @@ const UserProfile = () => {
               </td>
               <td className=" px-4 py-2 text-left [&[align=center]]:text-center [&[align=right]]:text-right">
                 {data?.email}
+              </td>
+            </tr>
+            <tr className="m-0 p-0">
+              <td className=" px-4 py-2 text-left [&[align=center]]:text-center [&[align=right]]:text-right">
+                Telegram
+              </td>
+              <td className=" px-4 py-2 text-left [&[align=center]]:text-center [&[align=right]]:text-right">
+                {data?.telegram_url}
               </td>
             </tr>
           </tbody>
