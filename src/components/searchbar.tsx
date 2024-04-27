52671150f821e707e995c2ef5219fb658ef5603c
Yeldar <57894795+issa012@users.noreply.github.com>
2024-04-27 11:56:58 +0500

    feat: search is now debounced

--- src/components/searchbar.tsx
+++ src/components/searchbar.tsx
@@ -1,7 +1,27 @@
+import { debounce } from "lodash";
+
 import { Search } from "lucide-react";
 import { Input } from "./ui/input";
+import { useCallback } from "react";
+import { useSearchParams } from "react-router-dom";
 
 export default function Searchbar() {
+  const [_, setSearchParams] = useSearchParams();
+
+  const debouncedOnChange = useCallback(
+    debounce((event: React.ChangeEvent<HTMLInputElement>) => {
+      event.preventDefault();
+
+      setSearchParams((prev) => {
+        if (event.target.value == "") prev.delete("search");
+        else prev.set("search", event.target.value);
+        prev.delete("page");
+        return prev;
+      });
+    }, 500),
+    []
+  );
+
   return (
     <div className="w-full flex-1">
       <form>
@@ -11,6 +31,7 @@ export default function Searchbar() {
             type="search"
             placeholder="Search by keyword..."
             className="w-full appearance-none bg-background pl-8 shadow-none md:w-2/3 lg:w-1/2"
+            onChange={debouncedOnChange}
           />
         </div>
       </form>
