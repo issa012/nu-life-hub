ce18a09df2392d7f959a29369ea8883a419f6603
Yeldar <57894795+issa012@users.noreply.github.com>
2024-04-27 13:59:42 +0500

    fix: default props

--- src/components/select-category.tsx
+++ src/components/select-category.tsx
@@ -11,19 +11,20 @@ import { Category } from "@/types";
 
 type CategorySelectProps = React.ComponentProps<typeof Select> & {
   categories: Category[] | undefined;
-  placeholder: string;
-  label: string;
+  placeholder: string | undefined;
+  label: string | undefined;
 };
 
 const CategorySelect = (props: CategorySelectProps) => {
+  const { placeholder = "Select category", label = "Category" } = props;
   return (
     <Select {...props}>
       <SelectTrigger className="w-full">
-        <SelectValue placeholder={props.placeholder} />
+        <SelectValue placeholder={placeholder} />
       </SelectTrigger>
       <SelectContent>
         <SelectGroup>
-          <SelectLabel>{props.label}</SelectLabel>
+          <SelectLabel>{label}</SelectLabel>
           <SelectItem value="all">Select a category</SelectItem>
           {props.categories?.map((category) => (
             <SelectItem value={"" + category.id} key={category.id}>
