0bac31c57a3e8e91f91255fc95ba6aa54150fb93
Yeldar <57894795+issa012@users.noreply.github.com>
2024-04-27 13:29:09 +0500

    fix: fix bug with search not having access to the latest state

--- src/components/searchbar.tsx
+++ src/components/searchbar.tsx
@@ -1,40 +1,39 @@
-import { debounce } from "lodash";
-
 import { Search } from "lucide-react";
 import { Input } from "./ui/input";
-import { useCallback } from "react";
+import { useState } from "react";
 import { useSearchParams } from "react-router-dom";
+import { useDebounce } from "@/hooks/useDebounce";
 
 export default function Searchbar() {
+  const [value, setValue] = useState("");
   const [_, setSearchParams] = useSearchParams();
 
-  const debouncedOnChange = useCallback(
-    debounce((event: React.ChangeEvent<HTMLInputElement>) => {
-      event.preventDefault();
+  const debouncedRequest = useDebounce(() => {
+    setSearchParams((prev) => {
+      prev.delete("page");
+      if (value == "") prev.delete("search");
+      else prev.set("search", value);
+      return prev;
+    });
+  });
 
-      setSearchParams((prev) => {
-        if (event.target.value == "") prev.delete("search");
-        else prev.set("search", event.target.value);
-        prev.delete("page");
-        return prev;
-      });
-    }, 500),
-    []
-  );
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value;
+    setValue(value);
+    debouncedRequest();
+  };
 
   return (
     <div className="w-full flex-1">
-      <form>
-        <div className="relative">
-          <Search className="absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground" />
-          <Input
-            type="search"
-            placeholder="Search by keyword..."
-            className="w-full appearance-none bg-background pl-8 shadow-none md:w-2/3 lg:w-1/2"
-            onChange={debouncedOnChange}
-          />
-        </div>
-      </form>
+      <div className="relative">
+        <Search className="absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground" />
+        <Input
+          type="search"
+          placeholder="Search by keyword..."
+          className="w-full appearance-none bg-background pl-8 shadow-none md:w-2/3 lg:w-1/2"
+          onChange={onChange}
+        />
+      </div>
     </div>
   );
 }
